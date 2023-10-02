a576ad095b6a88b06c3ba9b0cc5e26af394ac618
Ayoub Belhaj <ayoubbelhaj2017@gmail.com>
2023-10-02 22:47:40 +0100

    skillbar and responsive

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,11 +1,12 @@
 import Navbar from "./components/NavBar/navbar";
 import Intro from "./components/Intro/intro";
-
+import Skills from "./components/Skills/skills";
 function App() {
   return (
     <div className="App">
         <Navbar/>
         <Intro/>
+        <Skills/>
     </div>
   );
 }
